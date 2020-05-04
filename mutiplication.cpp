0b2b500f54e962acc58330b2271a9de8a38d3d0b
prasanth1367 <63450886+prasanth1367@users.noreply.github.com>
2020-05-04 17:00:27 +0530

    Add files via upload

---
+++ mutiplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y, z;
+    cout << "Enter two numbers: ";
+
+   
+    cin >> x >> y;
+ 
+    z = x * y;  
+
+    cout << "Product = " << z;    
+    
+    return 0;
+}
